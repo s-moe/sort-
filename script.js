e057e8bf0199e9d2d7ad7355f5c0dd191ecd739f
sarahmoe <>
2021-11-08 22:42:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort()
+
+console.log(fruits)
+
+const array1 = [1, 63, 89, 30, 4, 21, 500000, 100000000];
+array1.sort();
+console.log(array1)
